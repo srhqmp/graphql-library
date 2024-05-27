b3d68389cd45d55c3f4878b69a1af104d8aa006b
srhqmp <ballanosarah@gmail.com>
2024-05-27 22:42:41 +0800

    feat: addBook mutation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -120,6 +121,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]
     allAuthors: [Author!]
   }
+
+  type Mutation {
+    addBook(
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]
+    ):Book
+  }
 `;
 
 const resolvers = {
@@ -144,6 +154,18 @@ const resolvers = {
       );
     },
     allAuthors: () => authors,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      const author = {
+        name: args.author,
+        id: uuid(),
+      };
+      authors = authors.concat(author);
+      return book;
+    },
   },
   Book: {
     author: (root) => authors.find((a) => a.name === root.author),
